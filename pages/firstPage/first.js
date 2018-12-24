aa1320102011a1c199246ac5c8fb0ee7045b5aa0
qimingdai <1051234004@qq.com>
2018-12-24 20:53:49 +0800

    init

---
+++ pages/firstPage/first.js
@@ -0,0 +1,72 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    test : "这是文本内容" ,
+    btnText : "呵呵哒",
+    show : false,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+
+  btnClick: function(){
+    var isShow = this.data.show
+    this.setData({ test: "新的内容", show: !isShow})
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,34 @@
+
+Page({
+  data: {
+    imgUrls: [
+      '/image/01.jpg',
+      '/image/02.jpg',
+      '/image/03.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+  changeIndicatorDots(e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay(e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange(e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  }
+})
